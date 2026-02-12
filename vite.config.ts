04c72ad0c81797448c8f6d1369fad8b87b9f62b7
Ulgamon <ilicjustin@gmail.com>
2026-02-12 11:46:55 +0100

    added tailwind libraries

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // Alias @ to the src directory
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  assetsInclude: ["**/*.svg", "**/*.csv"],
+});
